5049c7a92f01b26c4bf30bf8b44a2baca81f00a9
burgokmen <burgokmen@gmail.com>
2023-11-07 02:06:31 +0300

    formpage border n submit button done

--- src/pages/RegisterFormPage.tsx
+++ src/pages/RegisterFormPage.tsx
@@ -1,36 +1,95 @@
 import { useForm } from "react-hook-form";
-
-type FormData = {
+type FormValues = {
+  userHandle: string;
   firstName: string;
   lastName: string;
+  email: string;
+  password: string;
+  cellular: string;
+  birthday: Date;
+  location: string;
+  profilePicture: string;
+  profileWalpaper: string;
 };
-
 export default function RegisterFormPage() {
   const {
     register,
-    setValue,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormValues>();
   const onSubmit = handleSubmit((data) => console.log(data));
-  // firstName and lastName will have correct type
+  console.log(errors);
 
   return (
-    <form className="flex flex-col" onSubmit={onSubmit}>
-      <label>First Name</label>
-      <input {...register("firstName")} />
-      <label>Last Name</label>
-      <input {...register("lastName")} />
-      <button
-        type="button"
-        onClick={() => {
-          setValue("lastName", "luo"); // ✅
-          setValue("firstName", true); // ❌: true is not string
-          errors.bill; // ❌: property bill does not exist
-        }}
-      >
-        SetValue
-      </button>
+    <form
+      className="flex flex-col m-auto border-8 p-5 w-[20rem]"
+      onSubmit={onSubmit}
+    >
+      <input
+        className="border-2 border-grey-500 "
+        type="text"
+        placeholder="User Handle"
+        {...register("userHandle", { maxLength: 40 })}
+      />
+      <input
+        className="border-2 border-grey-500 "
+        type="text"
+        placeholder="First name"
+        {...register("firstName", { required: true, maxLength: 80 })}
+      />
+      <input
+        className="border-2 border-grey-500 "
+        type="text"
+        placeholder="Last name"
+        {...register("lastName", { required: true, maxLength: 100 })}
+      />
+      <input
+        className="border-2 border-grey-500 "
+        type="text"
+        placeholder="Email"
+        {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+      />
+      <input
+        className="border-2 border-grey-500 "
+        type="password"
+        placeholder="Password"
+        {...register("password", { min: 8, maxLength: 150 })}
+      />
+      <input
+        className="border-2 border-grey-500 "
+        type="tel"
+        placeholder="Cellular"
+        {...register("cellular", { required: true, min: 10 })}
+      />
+      <input
+        className="border-2 border-grey-500 "
+        type="datetime"
+        placeholder="Birthday"
+        {...register("birthday", { valueAsDate: true })}
+      />
+      <input
+        className="border-2 border-grey-500 "
+        type="text"
+        placeholder="Location"
+        {...register("location", { maxLength: 30 })}
+      />
+      <input
+        className="border-2 border-grey-500 "
+        type="url"
+        placeholder="Profile Picture"
+        {...register("profilePicture")}
+      />
+      <input
+        className="mb-5 border-2 border-grey-500 "
+        type="url"
+        placeholder="ProfileWalpaper"
+        {...register("profileWalpaper")}
+      />
+
+      <input
+        className="bg-[#007bff] w-[10rem] text-white text-center p-2 rounded-md m-auto"
+        type="submit"
+      />
     </form>
   );
 }

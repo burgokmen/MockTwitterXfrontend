cf49e17efe572eed0f0a8c4d3b6d5c197ff01a67
burgokmen <burgokmen@gmail.com>
2023-11-06 18:29:46 +0300

    routes crated

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { Routes, Route } from "react-router-dom";
 import MainPage from "./layout/MainPage";
+import ProfilePage from "./pages/ProfilePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <>
-      <MainPage />
-    </>
+    <Routes>
+      <Route path="/" element={<RegisterPage />} />
+
+      <Route path="/login" element={<LoginPage />} />
+
+      <Route path="/home" element={<MainPage />} />
+
+      <Route path="/:userHandle" element={<ProfilePage />} />
+
+      <Route path="*" element={<RegisterPage />} />
+    </Routes>
   );
 }
 
